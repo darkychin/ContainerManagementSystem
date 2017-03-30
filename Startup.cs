14e02df920694c24f2f26a8ba422880b3b4b1552
darky <mr.wait_weishen@hotmail.com>
2017-03-30 14:41:45 +0800

    WebStruture

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContainerManagementSystem.Startup))]
+namespace ContainerManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
