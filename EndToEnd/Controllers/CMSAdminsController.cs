d9f0c883d871d8544f592d894da3f2d2cdb99d12
darky <mr.wait_weishen@hotmail.com>
2017-03-31 10:24:04 +0800

    CMS_Commit

---
+++ EndToEnd/Controllers/CMSAdminsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EndToEnd.Models;
+
+namespace EndToEnd.Controllers
+{
+    public class CMSAdminsController : Controller
+    {
+        private CMS_DatabaseEntities db = new CMS_DatabaseEntities();
+
+        // GET: CMSAdmins
+        public ActionResult Index()
+        {
+            return View(db.CMSAdmins.ToList());
+        }
+
+        // GET: CMSAdmins/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CMSAdmin cMSAdmin = db.CMSAdmins.Find(id);
+            if (cMSAdmin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cMSAdmin);
+        }
+
+        // GET: CMSAdmins/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CMSAdmins/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Gender,BOD,ContactNo,Email")] CMSAdmin cMSAdmin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CMSAdmins.Add(cMSAdmin);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cMSAdmin);
+        }
+
+        // GET: CMSAdmins/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CMSAdmin cMSAdmin = db.CMSAdmins.Find(id);
+            if (cMSAdmin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cMSAdmin);
+        }
+
+        // POST: CMSAdmins/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Gender,BOD,ContactNo,Email")] CMSAdmin cMSAdmin)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cMSAdmin).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cMSAdmin);
+        }
+
+        // GET: CMSAdmins/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CMSAdmin cMSAdmin = db.CMSAdmins.Find(id);
+            if (cMSAdmin == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cMSAdmin);
+        }
+
+        // POST: CMSAdmins/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CMSAdmin cMSAdmin = db.CMSAdmins.Find(id);
+            db.CMSAdmins.Remove(cMSAdmin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EndToEnd/Controllers/CargoesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EndToEnd.Models;
+
+namespace EndToEnd.Controllers
+{
+    public class CargoesController : Controller
+    {
+        private CMS_DatabaseEntities db = new CMS_DatabaseEntities();
+
+        // GET: Cargoes
+        public ActionResult Index()
+        {
+            var cargoes = db.Cargoes.Include(c => c.Customer).Include(c => c.Schedule).Include(c => c.Warehouse);
+            return View(cargoes.ToList());
+        }
+
+        // GET: Cargoes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargoes.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // GET: Cargoes/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name");
+            ViewBag.ScheduleId = new SelectList(db.Schedules, "Id", "Id");
+            ViewBag.WarehouseId = new SelectList(db.Warehouses, "Id", "Name");
+            return View();
+        }
+
+        // POST: Cargoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Destination,Status,CustomerId,RegisteredDate,Location,Quantity,ScheduleId,TotalWeight,WarehouseId")] Cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cargoes.Add(cargo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", cargo.CustomerId);
+            ViewBag.ScheduleId = new SelectList(db.Schedules, "Id", "Id", cargo.ScheduleId);
+            ViewBag.WarehouseId = new SelectList(db.Warehouses, "Id", "Name", cargo.WarehouseId);
+            return View(cargo);
+        }
+
+        // GET: Cargoes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargoes.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", cargo.CustomerId);
+            ViewBag.ScheduleId = new SelectList(db.Schedules, "Id", "Id", cargo.ScheduleId);
+            ViewBag.WarehouseId = new SelectList(db.Warehouses, "Id", "Name", cargo.WarehouseId);
+            return View(cargo);
+        }
+
+        // POST: Cargoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Destination,Status,CustomerId,RegisteredDate,Location,Quantity,ScheduleId,TotalWeight,WarehouseId")] Cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cargo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerId = new SelectList(db.Customers, "Id", "Name", cargo.CustomerId);
+            ViewBag.ScheduleId = new SelectList(db.Schedules, "Id", "Id", cargo.ScheduleId);
+            ViewBag.WarehouseId = new SelectList(db.Warehouses, "Id", "Name", cargo.WarehouseId);
+            return View(cargo);
+        }
+
+        // GET: Cargoes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cargo cargo = db.Cargoes.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // POST: Cargoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cargo cargo = db.Cargoes.Find(id);
+            db.Cargoes.Remove(cargo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
